3f948eb82a0b7e78a7993bc8346b61351cf43e3e
Francisco Sousa <franciscoeds1987@gmail.com>
2021-06-15 21:58:07 -0300

    Usando CrudRepository

---
+++ spring-data-jpa/spring-data/src/main/java/br/com/francisco/spring/data/repository/CargoRepository.java
@@ -0,0 +1,9 @@
+package br.com.francisco.spring.data.repository;
+
+import br.com.francisco.spring.data.orm.Cargo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Long> {
+}
